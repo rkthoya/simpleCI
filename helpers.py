1010f90c2dc15b0292a18803710a1e7f38ccf413
Kitsao Thoya <rkthoya@gmail.com>
2024-06-02 13:04:08 +0300

    Bug Fix

--- helpers.py
+++ helpers.py
@@ -3,7 +3,8 @@ import socket
 def communicate(host, port, request):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((host, port))
-    s.send(request)
+    request_bytes = request.encode()
+    s.send(request_bytes)
     response = s.recv(1024)
     s.close()
     return response
